b4c3a9e72bcf4774372c46dfedb4686ff6c56e11
Le Trong Dat <datlt2306@gmail.com>
2022-05-31 14:57:02 +0700

    update

--- javascript/bien.js
+++ javascript/bien.js
@@ -1,50 +1,29 @@
-// Biến
-// function
-// tên biến ->
-// - danh từ, isStatus
-
-// tên hàm nó phải hành động
-// - sum, showProducts, removeProduct
-
-// 1 tên biến - Danh từ
 const a = 10;
-const b = 20;
-
-// 3
-// tên hàm - Động từ: showProduct, removeProduct, checkEvenOrOddNumber()
-function sum(a, b) {
-    // kiem tra
-    if (typeof a !== "number" || typeof b !== "number") return -1;
-    // xu ly
-    const result = a + b;
-    // return
-    return result;
-}
-console.log(sum(10, 20));
-
-// vi du 2:
+const name = "Đạt";
+const isStatus = true;
+const hasChild = true;
+const running = function () {
+    console.log("running");
+};
+running();
 
 const productList = [
-    { id: 1, name: "Product A", price: 20 },
-    { id: 2, name: "Product B", price: 30 },
+    { id: 1, name: "Đat" },
+    { id: 2, name: "Kiên" },
 ];
 
-function showProducts(products) {
-    // kiểm tra
-    if (!Array.isArray(productList) || productList.length == 0) return [];
+console.log(productList[1].name);
 
-    // xử lý
-    let result = "";
-    for (let i = 0; i < products.length; i++) {
-        result += `<h2>${products[i]}</h2>`;
-    }
-    // return
-    return result;
-}
-function render(element) {
-    if (element) {
-        document.querySelector("#app").innerHTML = element;
-    }
-}
+// Đặt theo danh từ
+// trạng thái ( true /false ) thì thêm is/has
 
-render(showProducts(productList));
+// tên hàm: động từ
+const info = {
+    name: "Đạt",
+    isStatus: true,
+    running: function () {
+        console.log("running");
+    },
+};
+
+info.running();
