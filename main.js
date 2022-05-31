5335684a056bd109e36cf9d291c7e196ab3753f9
Le Trong Dat <datlt2306@gmail.com>
2022-05-31 10:38:59 +0700

    feat: list product

---
+++ main.js
@@ -0,0 +1,27 @@
+const productList = [
+    { id: 1, name: "product A", price: 200 },
+    { id: 2, name: "product B", price: 300 },
+];
+
+const productElement = document.querySelector("#product");
+
+function showProducts(products) {
+    // kiểm tra
+    if (!Array.isArray(productList) || productList.length == 0) return [];
+
+    // xử lý
+    let result = "";
+    for (let i = 0; i < products.length; i++) {
+        result += `<h2>${products[i].name}</h2>`;
+    }
+    // return
+    return result;
+}
+function render(element, content) {
+    console.log(content);
+    if (element) {
+        element.innerHTML = content;
+    }
+}
+render(productElement, showProducts(productList));
+// render(vi tri, noi dung);
